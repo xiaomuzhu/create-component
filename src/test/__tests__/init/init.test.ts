c1246b61857edb9138b54c0e2875205efd6678a2
xiaomuzhu <meituandxy@gmail.com>
2018-11-22 11:29:13 +0800

    test(src/test): add test for init command
    add test for init command

---
+++ src/test/__tests__/init/init.test.ts
@@ -0,0 +1,50 @@
+import * as execa from 'execa'
+import * as fs from 'fs-extra'
+import * as jetpack from 'fs-jetpack'
+import 'jest'
+import * as path from 'path'
+
+// const cwd = process.cwd()
+const TEST_DIR = 'testTemplateDir'
+const tempDir = path.join(process.cwd(), TEST_DIR)
+
+beforeEach(() => {
+  fs.ensureDir(tempDir)
+  process.chdir(tempDir)
+})
+
+test('spins up a min app and performs various checks', async () => {
+  await execa.shell(`create-component init ${TEST_DIR} -d -n`)
+
+  process.chdir(TEST_DIR)
+
+  const pkg = JSON.parse(jetpack.read('./package.json')!)
+
+  expect(pkg.name).toBe(TEST_DIR)
+
+  expect(pkg.version).toBe('1.0.0')
+
+  expect(pkg.husky).toBeUndefined()
+
+  const path = jetpack.exists('./.commitlintrc.js')
+
+  expect(path).toBeFalsy()
+})
+
+test('spins up a max app and performs various checks', async () => {
+  await execa.shell(`create-component init ${TEST_DIR} -f -n`)
+
+  process.chdir(TEST_DIR)
+
+  const pkg = JSON.parse(jetpack.read('./package.json')!)
+
+  expect(pkg.name).toBe(TEST_DIR)
+
+  expect(pkg.version).toBe('1.0.0')
+
+  expect(pkg.husky).toBeDefined()
+
+  const path = jetpack.exists('./.commitlintrc.js')
+
+  expect(path).toBeTruthy()
+})
